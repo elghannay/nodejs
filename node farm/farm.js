ccd3cf6bc88fd0343d9854b3d1f827dbb3918f18
elghannay <mohamedelghannay@gmail.com>
2020-05-29 11:14:46 +0000

    setting up dev dependencies

---
+++ node farm/farm.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%productName%}/g, product.productName);
+    output = output.replace(/{%id%}/g, product.id);
+    output = output.replace(/{%image%}/g, product.image);
+    output = output.replace(/{%from%}/g, product.from);
+    output = output.replace(/{%nutrients%}/g, product.nutrients);
+    output = output.replace(/{%quantity%}/g, product.quantity);
+    output = output.replace(/{%price%}/g, product.price);
+    output = output.replace(/{%description%}/g, product.description);
+    if (!product.organic)
+        output = output.replace(/{%is-organic%}/g, 'not-organic');
+    return output;
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    // true returns an object containing the query and additional info
+    // from which we extracted pathname and query
+    const { pathname, query } = url.parse(pathName, true);
+    // OVERVIEW
+
+
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        // replace the placeholders in the tempCard with the current product.  
+        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%product-cards%}', cardHtml);
+        res.end(output);
+    }
+    // PRODUCT
+
+
+    else if (pathname === '/product') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+    }
+    // API
+
+
+    else if (pathname === '/api') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(data);
+    }
+    // NOT FOUND
+
+
+    else {
+        res.writeHead(404, { 'content-type': 'text/html' });
+        res.end(`<h1>page not found</h1>`);
+    }
+})
+server.listen('8001', () => {
+    console.log('server running');
+})
+
---
+++ node farm/index.js
@@ -0,0 +1,85 @@
+////////////////////////////////////////////////
+/////////// playing with files /////////////////
+///////////////////////////////////////////////
+
+// const fs = require('fs');
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textOut = `this what we know about the avocado ${textIn} \n created on ${Date.now()}`
+// console.log(textOut);
+// fs.writeFileSync('./txt/out.txt', textOut)
+// // it is a bad practice to use synchronized functions in node.
+
+// fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+//     if (err)
+//         console.log('Error');
+//     console.log(data);
+//     fs.writeFile('./txt/inputsfeffsdsf.txt', data, (err, data) => {
+//         console.log('writing file ...');
+//         console.log(data);
+//     });
+// })
+// console.log('this code will run first');
+
+////////////////////////////////////////////////
+/////////// creating a server /////////////////
+///////////////////////////////////////////////
+
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     // when a request comes in, the server will respond by letting node firing an event
+//     const pathName = req.url;
+//     if (pathName === '/' || pathName === '/overview')
+//         res.end('you are requesting the overview page');
+//     else if (pathName === '/products')
+//         res.end('you are requesting the product page');
+//     else {
+//         // the order of these two instructions does matter.
+//         // always send the header before the content of the response
+//         res.writeHead(404, {
+//             'content-type': 'text/html',
+//             'myOwnContent': 'this is it'
+//         });
+//         // http header is a piece of information about the response that we are sending back 
+//         res.end(`<h1>page not found</h1>`);
+//     }
+//     res.end('here is a response from the server');
+// })
+// // listening for incoming requests on http://127.0.0.1:8000/ the host can be added 
+// // but in this case i have omitted it.
+// server.listen('8000', () => {
+//     console.log('server running');
+// })
+
+////////////////////////////////////////////////
+///////////   creating an Api  /////////////////
+///////////////////////////////////////////////
+
+const fs = require('fs');
+const http = require('http');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// console.log(JSON.parse(data));
+const server = http.createServer((req, res) => {
+    // the url method will parse the link parameter into a nice object
+    const pathName = req.url;
+    // a simplified routing logic that can be handled with Express js
+    if (pathName === '/' || pathName === '/overview')
+        res.end('you are requesting the overview page');
+    else if (pathName === '/products')
+        res.end('you are requesting the product page');
+    else if (pathName === '/api') {
+        res.writeHead(200, {
+            'content-type': 'application/json',
+        });
+        res.end(data);
+    }
+    else {
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'myOwnContent': 'this is it'
+        });
+        res.end(`<h1>page not found</h1>`);
+    }
+})
+server.listen('8000', () => {
+    console.log('server running');
+})
