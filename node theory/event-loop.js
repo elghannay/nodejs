8add960330639da2b233528216ec2fd709071a70
elghannay <mohamedelghannay@gmail.com>
2020-05-30 21:19:46 +0000

    event loop

---
+++ node theory/event-loop.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+process.env.UV_THREADPOOL_SIZE = 1;
+
+console.log('code from the top level code');
+setTimeout(() => {console.log('one second')}, 1000);
+setTimeout(() => { console.log('two second') }, 2000);
+// setImmediate runs first since they is no io callbacks
+setImmediate(() => { console.log('setImmediate', 2000) });
+// next tick is executed before all of them since it gets executed at the 
+// end of each phase in event loop
+process.nextTick(() => { console.log('next tick'); });
+fs.readFile('text.txt', () => { console.log('reading file')});
+
+// set the thread pool threads
+
+
